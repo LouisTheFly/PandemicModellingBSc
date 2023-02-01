77a425d4ccae56cbb8ef1eb4ddbefcd15782d336
LouisTheFly <65425636+LouisTheFly@users.noreply.github.com>
2023-02-01 15:10:13 +0000

    added infection

--- graph_generator.py
+++ graph_generator.py
@@ -10,88 +10,6 @@ import networkx as nx
 import numpy as np
 import scipy as sp
 
-
-#%%
-
-def complete_graph(dataframe):
-    nodes = list(dataframe.index)
-    for i in nodes:
-        temp_edges = []
-        for j in nodes:
-            if i != j:
-                temp_edges.append(j)
-        dataframe['edges'][i] = temp_edges
-    return
-
-def loop_graph(dataframe):
-    nodes = list(dataframe.index)
-    for i in nodes:
-        temp_edges = []
-        temp_edges.append((i+1)%(nodes[-1]+1))
-        temp_edges.append((i-1)%(nodes[-1]+1))
-        dataframe['edges'][i] = temp_edges
-    return
-
-def complete_rand_sub(dataframe):
-    nodes = list(dataframe.index)
-    edge_tot = 0
-    for i in nodes:
-        temp_edges = []
-        for j in nodes:
-            if i != j:
-                temp_edges.append(j)
-                edge_tot += 1
-        dataframe['edges'][i] = temp_edges
-    return
-
-def degree_calc(dataframe):
-    for i in dataframe.index:
-        dataframe['degree'][i] = len(dataframe['edges'][i])
-    return
-    
-#########################################################
-
-
-def draw_circular_graph(dataframe):
-    nodes = list(dataframe.index)
-    G = nx.Graph()
-    G.add_nodes_from(nodes)
-    for i in nodes:
-        for j in dataframe['edges'][i]:
-            G.add_edge(i,j)
-    nx.draw_circular(G, with_labels = True)
-    return
-
-def draw_random_graph(dataframe):
-    nodes = list(dataframe.index)
-    G = nx.Graph()
-    G.add_nodes_from(nodes)
-    for i in nodes:
-        for j in dataframe['edges'][i]:
-            G.add_edge(i,j)
-    nx.draw_random(G, with_labels = True)
-    return
-    
-
-#%%
-numberofnodes=12
-nodes = np.arange(numberofnodes)
-Columns=('degree','edges','prob')
-
-dataframe=pd.DataFrame(data=None,index=nodes,columns=Columns)
-
-test = dataframe
-
-#Type of graph
-complete_graph(test)
-loop_graph(test)
-degree_calc(test)
-
-#Visual Graph
-#draw_circular_graph(test)
-draw_random_graph(test)
-
-
 #%% Generating graphs in newtwork x
 
 #Dataframe generation from graph
@@ -117,22 +35,32 @@ def make_graph(nodes = 10, graph_type = 'complete', draw_type = 'circular', show
     #Add edge attributes
     nx.set_edge_attributes(G, 0.5, name = 'Probability')
     
+    #Define shape of plot
+    #'nx.'+
+    pos = nx.draw_type_layout(G)
+    #Show a plot of the graph if needed
+    if show == True:
+           nx.draw_networkx(G, pos, with_labels = True)
+           nx.draw_networkx_nodes(G, pos, node_color="tab:blue")
+           #nx.draw_networkx_nodes(G, pos, nodelist=[0, 1, 2, 3], node_color="tab:red")
+           nx.draw_networkx_edges(G, pos)
+    
     #Make a dataframe with all information within the graph
     if dataset == True:
         df = make_dataframe(G)
         return df
     
-    #Show a plot of the graph if needed
-    if show == True:
-        if draw_type == 'random':
-            nx.draw_random(G, with_labels = True)
-        else:
-            nx.draw_circular(G, with_labels = True)
+    return G
+
+def infection(G, nodes_to_infect):
+    nodes = dict.fromkeys(nodes_to_infect, 1)
+    nx.set_node_attributes(G, nodes, name = 'Infection')
     return G
 
 #%%
-graph_test = make_graph(15, graph_type = 'cycle', dataset = False)
+graph_test = make_graph(15, graph_type = 'cycle')
 #df_test = make_dataframe(graph_test)
+<<<<<<< Updated upstream
 
 
 #%%
@@ -150,3 +78,6 @@ def returninfections(array_probabilities,nodes):
     
     
     
+=======
+#list(nx.neighbors(graph_test, 7))
+>>>>>>> Stashed changes
