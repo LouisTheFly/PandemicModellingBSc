1e59adf0c16d42e5583cd6e090d412bb6d670671
LouisTheFly <65425636+LouisTheFly@users.noreply.github.com>
2023-02-01 13:12:35 +0000

    graph generator

--- graph_generator.py
+++ graph_generator.py
@@ -32,6 +32,18 @@ def loop_graph(dataframe):
         dataframe['edges'][i] = temp_edges
     return
 
+def complete_rand_sub(dataframe):
+    nodes = list(dataframe.index)
+    edge_tot = 0
+    for i in nodes:
+        temp_edges = []
+        for j in nodes:
+            if i != j:
+                temp_edges.append(j)
+                edge_tot += 1
+        dataframe['edges'][i] = temp_edges
+    return
+
 def degree_calc(dataframe):
     for i in dataframe.index:
         dataframe['degree'][i] = len(dataframe['edges'][i])
@@ -79,3 +91,45 @@ degree_calc(test)
 #draw_circular_graph(test)
 draw_random_graph(test)
 
+
+#%% Generating graphs in newtwork x
+
+#Dataframe generation from graph
+def make_dataframe(graph):
+    index = np.arange(len(graph))
+    columns=('degree','edges','prob')
+    df=pd.DataFrame(data=None,index=index,columns=columns)
+    #df['degree'] = nx.degree(graph)
+    return df
+
+
+def make_graph(nodes = 10, graph_type = 'complete', draw_type = 'circular', show = True, dataset = False):
+    
+    #Choose which graph to draw
+    if graph_type == 'cycle':
+        G = nx.cycle_graph(nodes)
+    else:
+        G = nx.complete_graph(nodes)
+    
+    #Add node attributes
+    nx.set_node_attributes(G, 0, name = 'Infection')
+    
+    #Add edge attributes
+    nx.set_edge_attributes(G, 0.5, name = 'Probability')
+    
+    #Make a dataframe with all information within the graph
+    if dataset == True:
+        df = make_dataframe(G)
+        return df
+    
+    #Show a plot of the graph if needed
+    if show == True:
+        if draw_type == 'random':
+            nx.draw_random(G, with_labels = True)
+        else:
+            nx.draw_circular(G, with_labels = True)
+    return G
+
+#%%
+graph_test = make_graph(15, graph_type = 'cycle', dataset = False)
+#df_test = make_dataframe(graph_test)
