c1e132b9d46801acb8907d75064e266a5683beeb
LouisTheFly <65425636+LouisTheFly@users.noreply.github.com>
2023-02-02 12:49:07 +0000

    fixing draw graph

--- graph_generator.py
+++ graph_generator.py
@@ -39,13 +39,16 @@ def make_graph(nodes = 10, graph_type = 'complete'):
 
 def draw_graph(G, draw_type = 'circular'):
     #Define shape of plot
-    'nx.'+ draw_type +'_layout(G)'
-    pos = nx.draw_type_layout(G)
+    pos = exec('nx.%s_layout(%s)'%(draw_type, G))
+    print(pos)
+    #print('nx.'+ draw_type +'_layout(G)')
+    #pos = nx.draw_type_layout(G)
     
     nx.draw_networkx(G, pos, with_labels = True)
-    nx.draw_networkx_nodes(G, pos, node_color="tab:blue")
+    #nx.draw_networkx_nodes(G, pos, node_color="tab:blue")
     #nx.draw_networkx_nodes(G, pos, nodelist=[0, 1, 2, 3], node_color="tab:red")
-    nx.draw_networkx_edges(G, pos)
+    #nx.draw_networkx_edges(G, pos)
+    return
 
 
 def infection(G, nodes_to_infect):
@@ -54,9 +57,9 @@ def infection(G, nodes_to_infect):
     return G
 
 #%%
-graph_test = make_graph(15, graph_type = 'cycle')
+graph_test = make_graph(10, graph_type = 'cycle')
 #df_test = make_dataframe(graph_test)
-
+draw_graph(graph_test, draw_type = 'circular')
 
 
 #%%
