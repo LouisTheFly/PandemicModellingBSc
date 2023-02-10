19ddaf4c2c3a2be9466f2d9c05297d161f0f0dde
LouisTheFly <65425636+LouisTheFly@users.noreply.github.com>
2023-02-10 14:44:38 +0000

    planning doc update

--- data_collections.py
+++ data_collections.py
@@ -17,17 +17,17 @@ import algorithm as alg
 #%% Testing
 
 ##########Setup#########
-time_steps = 200
-show = False
+time_steps = 1000
+show = True
 log = False
 
 nodes = 25
 graph_type = 'WS'
-base_edge_prob = 0.1
+base_edge_prob = 0.01
 nodes_to_infect = [0]
-nodes_to_vaccinate = [3,7]
+nodes_to_vaccinate = [3,7,13]
 
-graph = gen.make_graph(nodes = 25, graph_type = 'cycle', base_edge_prob = 0.5) # dataset = False
+graph = gen.make_graph(nodes = nodes, graph_type = graph_type, base_edge_prob = base_edge_prob) # dataset = False
 
 #Infect nodes
 graph = alg.infect_nodes(graph, nodes_to_infect)
@@ -36,12 +36,12 @@ graph = alg.infect_nodes(graph, nodes_to_infect)
 graph = alg.vaccinate_nodes(graph, nodes_to_vaccinate)
 
 #Drawing
-#gen.draw_graph(graph, draw_type = 'circular')
+gen.draw_graph(graph, draw_type = 'circular')
 
 ########Iterating########
 
-graph,infectedlist,infectionsperday=alg.run_graph(graph, time_steps, show = False, log = False, delay = False)
+graph,infectedlist,infectionsperday=alg.run_graph(graph, time_steps, show = show, log = log, delay = False)
 
 ########Graphing########
 
-alg.plotting(np.arange(time_steps),infectionsperday, 'bar', 'Infections per Day, Tot = %s'%(len(infectedlist)), 'Time (in days)', 'Number of Infections')
+#alg.plotting(np.arange(time_steps),infectionsperday, 'bar', 'Infections per Day, Tot = %s'%(len(infectedlist)), 'Time (in days)', 'Number of Infections')
