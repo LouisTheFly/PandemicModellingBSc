193ba80e44daadb7feb63a750e7a6984de7e9998
Lineydp <115625322+Lineydp@users.noreply.github.com>
2023-02-10 18:39:20 +0000

    Update data_collections.py

--- data_collections.py
+++ data_collections.py
@@ -17,23 +17,38 @@ import algorithm as alg
 #%% Testing
 
 ##########Setup#########
+time_steps = 10
+show = True
+log = False
 
+<<<<<<< HEAD
 graph = gen.make_graph(10000, graph_type = 'WS', base_edge_prob = 0.01) # dataset = False
+=======
+nodes = 10
+graph_type = 'WS'
+base_edge_prob = 1
+nodes_to_infect = [0]
+#nodes_to_vaccinate = [5,7]
+amount_to_vaccinate = 4
+
+graph = gen.make_graph(nodes = nodes, graph_type = graph_type, base_edge_prob = base_edge_prob) # dataset = False
+>>>>>>> 4a5d605f691d77ec9518fc9c330e067c6df34d2d
 
 #Infect nodes
-graph = alg.infect_nodes(graph, [0])
+graph = alg.infect_nodes(graph, nodes_to_infect)
 
 #Vaccinate nodes
-#graph = alg.vaccinate_nodes(graph, [1,7,16,11,20,22])
+#graph = alg.vaccinate_nodes(graph, nodes_to_vaccinate)
+graph = alg.vaccinate_random_nodes(graph, amount_to_vaccinate)
 
 #Drawing
-#gen.draw_graph(graph, draw_type = 'circular')
+gen.draw_graph(graph, draw_type = 'circular')
 
+#%%
 ########Iterating########
 
-time=1000
-graph,infectedlist,infectionsperday=alg.run_graph(graph, time, show = False, log = False, delay = False)
+graph,infectedlist,infectionsperday=alg.run_graph(graph, time_steps, show = show, log = log, delay = False)
 
 ########Graphing########
 
-alg.plotting(np.arange(time),infectionsperday, 'bar', 'Infections per Day, Tot = %s'%(len(infectedlist)), 'Time (in days)', 'Number of Infections')
+#alg.plotting(np.arange(time_steps),infectionsperday, 'bar', 'Infections per Day, Tot = %s'%(len(infectedlist)), 'Time (in days)', 'Number of Infections')
