eaa46a213a498a5e067d5fc0971e4b9821a0f6df
LouisTheFly <65425636+LouisTheFly@users.noreply.github.com>
2023-03-07 16:42:19 +0000

    Todo list + random vaccination

--- data_collections.py
+++ data_collections.py
@@ -18,22 +18,24 @@ import algorithm as alg
 
 ##########Setup#########
 time_steps = 80
-show = False
+show = True
 log = False
 plot = True
 five_day_average = True
 
-nodes = 1000
+nodes = 20
 graph_type = 'WS'
 base_edge_prob = 0.1
-nodes_to_infect = [0]
+#nodes_to_infect = [0]
+amount_to_infect = 4
 #nodes_to_vaccinate = [5,7]
-amount_to_vaccinate = 500
+amount_to_vaccinate = 5
 
 graph = gen.make_graph(nodes = nodes, graph_type = graph_type, base_edge_prob = base_edge_prob) # dataset = False
 
 #Infect nodes
-graph = alg.infect_nodes(graph, nodes_to_infect)
+#graph = alg.infect_nodes(graph, nodes_to_infect)
+graph = alg.infect_random_nodes(graph, amount_to_infect)
 
 #Vaccinate nodes
 #graph = alg.vaccinate_nodes(graph, nodes_to_vaccinate)
@@ -42,7 +44,6 @@ graph = alg.vaccinate_random_nodes(graph, amount_to_vaccinate)
 #Drawing
 if show == True:
     gen.draw_graph(graph, draw_type = 'circular')
-
 
 ########Iterating########
 
